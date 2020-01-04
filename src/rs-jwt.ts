7f3975c2ceba3343584e4befbaedace67fa9b8d1
RobDWaller <rdwaller1984@googlemail.com>
2020-01-04 18:09:32 +0000

    Added split token method to RSJwt class and added associated tests.

--- src/rs-jwt.ts
+++ src/rs-jwt.ts
@@ -2,4 +2,14 @@ export class RSJwt {
   getPayload(): string {
     return 'Hello World';
   }
+
+  splitToken(token: string) {
+    let parts = token.split(".");
+
+    return {
+      header: parts[0],
+      payload: parts[1],
+      signature: parts[2]
+    }
+  }
 }
--- tests/test.rs-jwt.ts
+++ tests/test.rs-jwt.ts
@@ -2,7 +2,7 @@ import { RSJwt } from '../src/rs-jwt';
 import { expect } from 'chai';
 import 'mocha';
 
-describe('Random Number List', () => {
+describe('Really Simple JWT', () => {
 
   it('Should return a JWT payload object', () => {
     let jwt = new RSJwt();
@@ -10,5 +10,15 @@ describe('Random Number List', () => {
     let result = jwt.getPayload();
     
     expect(result).to.equal('Hello World');
+  });
+
+  it('Should split a JWT and return the three parts.', () => {
+    let jwt = new RSJwt();
+
+    let result = jwt.splitToken('abc.def.hij');
+
+    expect(result.header).to.equal('abc');
+    expect(result.payload).to.equal('def');
+    expect(result.signature).to.equal('hij');
   });
 });
