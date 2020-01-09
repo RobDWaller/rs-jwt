f6605d7574d474c09046e55b1d48ca6529e5ba61
RobDWaller <rdwaller1984@googlemail.com>
2020-01-09 13:49:42 +0000

    Create JWT Object class to contain decoded JWT token string.

---
+++ src/jwt.ts
@@ -0,0 +1,25 @@
+export class Jwt {
+  private header: Object
+
+  private payload: Object
+
+  private signature: string | null
+
+  constructor (header: Object, payload: Object, signature: string | null) {
+    this.header = header
+    this.payload = payload
+    this.signature = signature
+  }
+
+  getHeader(): Object {
+    return this.header
+  }
+
+  getPayload (): Object {
+    return this.payload
+  }
+
+  getSignature (): string | null {
+    return this.signature
+  }
+}
---
+++ tests/test.jwt.ts
@@ -0,0 +1,41 @@
+import { Jwt } from '../src/jwt'
+import { expect, assert } from 'chai'
+import { describe, it } from 'mocha'
+
+describe('JWT Object', () => {
+  it('Should return a header object', () => {
+    const header = {} 
+    const payload = {}
+    const signature = ''
+    
+    const jwt = new Jwt(header, payload, signature)
+
+    const result = jwt.getHeader()
+
+    assert.instanceOf(result, Object)
+  })
+
+  it('Should return a payload object', () => {
+    const header = {} 
+    const payload = {}
+    const signature = ''
+    
+    const jwt = new Jwt(header, payload, signature)
+
+    const result = jwt.getPayload()
+
+    assert.instanceOf(result, Object)
+  })
+
+  it('Should return a signature string', () => {
+    const header = {} 
+    const payload = {}
+    const signature = '123'
+    
+    const jwt = new Jwt(header, payload, signature)
+
+    const result = jwt.getSignature()
+
+    expect(result).to.equal('123')
+  })
+})
