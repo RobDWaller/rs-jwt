5224c027ae9e8f23ea35645a5a89fdfb642554a2
RobDWaller <rdwaller1984@googlemail.com>
2020-01-30 14:58:42 +0000

    Updated index.ts to provide facade wrapper function around RSJwt parsing functionality.

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,12 @@
 import { RSJwt } from './rs-jwt'
 import { Jwt } from './jwt'
 
+/**
+ * Parse a JSON Web Token to a JavaScript object which allows you to easily
+ * access claims in the header and payload.
+ * 
+ * @author Rob Waller <rdwaller1984@gmail.com>
+ */
 export function parseJwt (token: string): Jwt {
   const jwt = new RSJwt()
 
